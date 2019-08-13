52300628a5a5167ca56ef9f13677a59c83ea82e8
Daniel Saewitz <switz213@gmail.com>
2019-08-13 14:58:09 -0400

    Fix indentation

--- typescript.js
+++ typescript.js
@@ -1,19 +1,21 @@
 module.exports = {
   env: {
-    browser: false,
+    browser: true,
     es6: true,
     node: true,
   },
   extends: [
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    './eslintrc.js'
+    './eslintrc.js',
   ],
   rules: {
+    'indent': 'off',
+    '@typescript-eslint/indent': ['error', 2],
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    'ecmaFeatures.jsx': true
+    'ecmaFeatures.jsx': true,
   },
-  plugins: ["@typescript-eslint"]
+  plugins: ['@typescript-eslint'],
 };
