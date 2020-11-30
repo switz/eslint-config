6f2baf18f169fafc675eb84d77d93cc474dc5ad4
Daniel Saewitz <switz213@gmail.com>
2020-11-30 01:16:03 -0500

    Ignore rest vars

--- typescript.js
+++ typescript.js
@@ -13,7 +13,7 @@ module.exports = {
   rules: {
     'indent': 'off',
     'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
     '@typescript-eslint/indent': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/camelcase': 'off',
