96cea31a918bb58bb3ab007f13fa238f67403184
Daniel Saewitz <git@saewitz.com>
2025-04-06 16:06:53 -0400

    Fixes

--- typescript.mjs
+++ typescript.mjs
@@ -1,19 +1,15 @@
 import main from './eslint.config.mjs';
 import tseslint from 'typescript-eslint';
 
-export default [
-  ...main,
-  tseslint.configs.recommended,
-  {
-    rules: {
-      'indent': 'off',
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
-      '@typescript-eslint/indent': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/camelcase': 'off',
-      '@typescript-eslint/no-use-before-define': ['error', { variables: false }],
-      '@typescript-eslint/no-empty-function': 'off',
-    },
+export default tseslint.config(main, tseslint.configs.recommended, {
+  rules: {
+    'indent': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/no-use-before-define': ['error', { variables: false }],
+    '@typescript-eslint/no-empty-function': 'off',
   },
-];
+});
