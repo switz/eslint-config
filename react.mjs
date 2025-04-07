c8a7b69f7cef27ee34a49c7e2c9229e01697c3d1
Daniel Saewitz <git@saewitz.com>
2025-04-07 13:16:06 -0400

    Remove tailwind for now..

--- react.mjs
+++ react.mjs
@@ -1,12 +1,12 @@
 import react from 'eslint-plugin-react';
 import main from './eslint.config.mjs';
-import tailwind from './tailwind.mjs';
+// import tailwind from './tailwind.mjs';
 
 export default [
   react.configs.flat.recommended,
   react.configs.flat['jsx-runtime'],
   ...main,
-  ...tailwind,
+  // ...tailwind,
   {
     rules: {
       'react/display-name': 'off',
